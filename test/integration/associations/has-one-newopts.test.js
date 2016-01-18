9709d571d24280de4e3b8ace6082a415ca6aa97d
Robert Grimes <rmzgrimes@gmail.com>
2016-01-18 01:46:13 -0600

    Initial belongs-to association update.

--- test/integration/associations/has-one-newopts.test.js
+++ test/integration/associations/has-one-newopts.test.js
@@ -2,42 +2,44 @@
 
 /* jshint -W030 */
 var chai = require('chai')
+  , Support = require(__dirname + '/../support')
   , expect = chai.expect
 	, Association = require('../../../lib/associations/index');
 
-describe('describe test', function() {
-	it('it test', function() {
-		var User = this.sequelize.define('User', {})
-			, Addr = this.sequelize.define('Address', {});
+describe(Support.getTestDialectTeaser('BelongsTo'), function() {
+	describe('describe test', function() {
+		it('it test', function() {
+			var User = this.sequelize.define('User', {})
+				, Addr = this.sequelize.define('Address', {});
 
-		var assoc = User.hasOne(Addr, {foreignKey: 'test_key'});
+			var assoc = User.hasOne(Addr, {foreignKey: 'test_key'});
 
-		return this.sequelize.sync({force: true}).then(function(){
-			//expect(User.sourceKey).to.equal('id');
-			//expect(User.targetKey).to.equal('test_key');
-			
-			expect(assoc).to.be.an.instanceOf(Association);
-			expect(assoc.sourceKey).to.equal('id');
-			expect(assoc.targetKey).to.equal('test_key');
+			return this.sequelize.sync({force: true}).then(function(){
+				//expect(User.sourceKey).to.equal('id');
+				//expect(User.targetKey).to.equal('test_key');
+				
+				expect(assoc).to.be.an.instanceOf(Association);
+				expect(assoc.sourceKey).to.equal('id');
+				expect(assoc.targetKey).to.equal('test_key');
+			});
+		});
+	});
+
+	describe('describe test', function() {
+		it('it test', function() {
+			var User = this.sequelize.define('User', {})
+				, Addr = this.sequelize.define('Group', {});
+
+			var assoc = User.hasOne( Addr, {sourceKey: 'name', targetKey: 'user_name'} );
+
+			return this.sequelize.sync({force: true}).then(function(){
+				expect(assoc).to.be.an.instanceOf(Association);
+				expect(assoc.sourceKey).to.equal('name');
+				expect(assoc.targetKey).to.equal('user_name');
+			});
+
+
+			//expect(Object.keys(Group.associations)).to.deep.equal(['User', 'primaryUsers', 'secondaryUsers']);
 		});
 	});
 });
-
-describe('describe test', function() {
-	it('it test', function() {
-		var User = this.sequelize.define('User', {})
-			, Addr = this.sequelize.define('Group', {});
-
-		var assoc = User.hasOne( Addr, {sourceKey: 'name', targetKey: 'user_name'} );
-
-		return this.sequelize.sync({force: true}).then(function(){
-			expect(assoc).to.be.an.instanceOf(Association);
-			expect(assoc.sourceKey).to.equal('name');
-			expect(assoc.targetKey).to.equal('user_name');
-		});
-
-
-		//expect(Object.keys(Group.associations)).to.deep.equal(['User', 'primaryUsers', 'secondaryUsers']);
-	});
-});
-
